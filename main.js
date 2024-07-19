08a33e7ca0b2b3f3e2128fc05c584bad4b25f756
josungwong <110151451+josungwong@users.noreply.github.com>
2024-07-19 20:01:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 let dataList = [];
 let showList = []
 let popUpScreen = document.getElementById("pop-box")
-let titleText = document.querySelectorAll(".show-exp")
-titleText.forEach((E)=>
-E.addEventListener("click",(event) => popText(event)))
+let page = 0
 
 const url = new URL(
   `https://stargolf.info/API_TEST/get_tasks.php?limit=111`
@@ -48,69 +46,55 @@ const showRender = () => {
               <img class="show-img-size" src=${show.wr_link1} onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUnvISVTYopMAy17o3mB2lfSPeEjoKfAdV2w&s';">
               </a>
           </div>
-          <div class="show-exp" onclick="popUp()">
+          <div class="show-exp" onclick="popUp();popText('${show.wr_link1}','${show.wr_link2}','${show.wr_subject}','${show.wr_1}','${show.wr_3}','${show.wr_4}','${show.wr_6}','${show.wr_10}')">
               <div id="show-title">
-                  <a href=${show.wr_10} class="none-a"><h4 class="none-a">${show.wr_subject}</h4></a>
+                  <h4 class="none-a">${show.wr_subject}</h4>
               </div>
-              
-             
           </div>
       </div>`    
     ).join('')
 
-// <div id="show-time">
-//   <p>${showTime(show)}</p>
-// </div>
-// <div id="show-address">
-//     <p>address: ${showAddress(show)}</p>
-// </div>
+
   document.getElementById("show-box").innerHTML = showHTML
 }
 
 
-// const showTime = (show)=>{
-//   let arr = ""
-//   arr = show.wr_4
-//   if(arr == ""){
-//       return show = "unknown"
-//   }else{
-//       if(arr.length > 20){
-//           return arr = `time: ${arr.substring(0,15) + "..."}`
-//       }else{
-//           return arr = `time: ${arr}` 
-//       }
+const popUp = () =>{
+  popUpScreen.style.display = "block"
 
-//   }
-// } 
+}
 
-// const showAddress = (show) =>{
-//   let arr1 = ""
-//   arr1 = show.wr_6
-//   if(arr1.length > 20){
-//       return arr1.substring(0,15) + "..."
-//   }else{
-//       return arr1 
-//   }
-// }
+const exit = ()=>{
+  popUpScreen.style.display = "none"
+}
 
-// const popUp = () =>{
-//   popUpScreen.style.display = "block"
-
-// }
-
-// const exit = ()=>{
-//   popUpScreen.style.display = "none"
-// }
-
-// const popText = (event) => {
-//   console.log(event)
-//   for(let i = 0; i < showList.length;i++){
-//     console.log(titleText[i])
-
-//   }
-//   console.log("클릭")
+const popText = (img1,img2,title,type,phone,time,address,homepage) => {
   
-  //각 내용을 상세히
-  // document.getElementById("pop-inner").innerHTML = popupHTML
-// }
-console.log("그림을 클릭하면 사이트로 들어가고 이름을 클릭하면 창을 띄어서 거기에서 자세히 설명해주고 싶은데 이걸 구연할 방법을 모집중입니다")
+  console.log("클릭",img1,img2,title,type,phone,time,address,homepage)
+  
+  let popupHTML = `<div>
+          <div>
+              <a id="show-img" href=${homepage}>
+              <img class="show-img-size block" src=${img1} onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUnvISVTYopMAy17o3mB2lfSPeEjoKfAdV2w&s';">
+              <img class="show-img-size block " src=${img2} https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUnvISVTYopMAy17o3mB2lfSPeEjoKfAdV2w&s>
+              </a>
+          </div>
+          <div class="show-exp" >
+              <div id="show-title">
+                  <h5>${title}</h5>
+                  <p>${type}</p>
+                  <p>${time}</p>
+                  <p>${phone}</p>
+                  <p>${address}</p>
+              </div>
+          </div>
+      </div>` 
+
+
+
+  document.getElementById("pop-inner").innerHTML = popupHTML
+}
+
+const pageCount = () =>{
+  //여기에 만들려고 하고있어요
+}
